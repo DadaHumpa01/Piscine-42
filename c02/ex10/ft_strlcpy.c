3f77fdebe6d4123f609a3a8601d0884662a6d772
DadaHumpa01 <74679282+DadaHumpa01@users.noreply.github.com>
2020-11-18 23:56:28 +0100

    Create ft_strlcpy.c

---
+++ c02/ex10/ft_strlcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbrignon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/02 18:23:07 by dbrignon          #+#    #+#             */
+/*   Updated: 2020/11/02 18:53:31 by dbrignon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int lenght;
+	unsigned int i;
+
+	lenght = 0;
+	i = 0;
+	while (src[lenght] != '\0')
+		lenght++;
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (lenght);
+}
