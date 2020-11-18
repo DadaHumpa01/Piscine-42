c2a544fa8d9c7a7ce8006d185347be38def787db
DadaHumpa01 <74679282+DadaHumpa01@users.noreply.github.com>
2020-11-19 00:00:49 +0100

    Create ft_strncat.c

---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbrignon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/05 18:11:42 by dbrignon          #+#    #+#             */
+/*   Updated: 2020/11/05 18:17:04 by dbrignon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int i;
+	unsigned int j;
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+		i++;
+	while (src[j] && j < nb)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
